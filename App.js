9cf0f5279f69043d1ab8dc131dcd3c773598d6a8
Aditya Jani <44555223+adi07rak@users.noreply.github.com>
2024-12-11 08:31:19 -0600

    Updated changes till V74

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import AppLoading from 'expo-app-loading';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const [fontsLoaded] = useFonts({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -31,14 +32,25 @@ export default function App() {
     setGameIsOver(true);
   }
 
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if(userNumber) {
     screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
   }
 
-  if(gameIsOver && userNumber) {
-    screen= <GameOverScreen />
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
   }
 
 
